c8a2643f67f2ea0ddb83e64f0b16e2afc35095b8
NOTaMIME-jk <60108401+NOTaMIME-jk@users.noreply.github.com>
2022-05-11 11:26:38 +0100

    Add files via upload
    Some of my first code in bash, just plying around with the language and git

---
+++ getritchquick.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+echo "What is your name?"
+read name
+
+echo "How old are you?"
+read age
+
+milionareage=$(( ( $RANDOM % 90 ) + $age ))
+
+echo "Hello $name, you are $age years old."
+sleep 2
+echo "You will become a milionare at $milionareage yo"
+sleep 2
+echo "You will be ritch in $(((milionareage - age) * 365)) days $name."
+sleep 2
