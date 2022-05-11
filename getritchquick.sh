2b6507bf8143d573f5e3ead681efa859fcb19653
NOTaMIME-jk <60108401+NOTaMIME-jk@users.noreply.github.com>
2022-05-11 11:33:57 +0100

    Update getritchquick.sh

--- getritchquick.sh
+++ getritchquick.sh
@@ -10,7 +10,17 @@ milionareage=$(( ( $RANDOM % 90 ) + $age ))
 
 echo "Hello $name, you are $age years old."
 sleep 2
+
 echo "You will become a milionare at $milionareage yo"
 sleep 2
+
+echo "."
+sleep 1
+echo ".."
+sleep 1
+echo "..."
+sleep 1
+echo "...."
+
 echo "You will be ritch in $(((milionareage - age) * 365)) days $name."
 sleep 2
