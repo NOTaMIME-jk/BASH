aa62c91ae3f4b866fdbf8393474c30da2f70f3e6
NOTaMIME-jk <60108401+NOTaMIME-jk@users.noreply.github.com>
2022-05-12 18:04:04 +0100

    Update if-statement.sh

--- if-statement.sh
+++ if-statement.sh
@@ -1,10 +1,42 @@
 #! /bin/bash
 
-echo "What is your name?"
-read name
+echo "Do you want to make? "
+echo "a) pyramid pyramid"
+echo "b) half a pyramid pyramid"
+read answer
 
-if [ $name ]; then 
-	echo "$name sounds ritch to me"
+if [ $answer = 'a' ]; then 
+	echo "Enter a number pls"
+	read rows
+	for (( i=0; i<=rows; i++))
+	do	
+        	for ((j=rows; j>=i; j--))
+        	do
+                	echo -n "    "
+       		done
+
+        for ((j=1; j<=i; j++))
+        do
+                echo -n "pyramid "
+        	done
+        	echo
+	done
+	echo
+	
+elif [ $answer = 'b' ]; then
+	echo "Enter a number pls"
+
+	read rows
+	for (( i=0; i<=rows; i++))
+	do
+        	for ((j=1; j<=i; j++))
+        	do
+                	echo -n "pyramid "
+        	done
+        	echo
+	done
+	echo
+
 else
 	echo "Doesn't sound like anything to me."
 fi
